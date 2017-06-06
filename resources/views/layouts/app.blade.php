355e522e11719bb9ae77156b35dd33bc8bc7d0da
Serge Benard <serge.benard@gmail.com>
2017-06-06 18:06:14 -0400

    Fix for small bugs

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Styles 
+        <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+        -->
+        
+        <!-- Scripts -->
+        <script>
+        window.Laravel = {!! json_encode([
+            'csrfToken' => csrf_token(),
+        ]) !!};
+        </script>
+
+        <title>@yield('pageTitle')</title>
+
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Font Awesome CSS -->
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
+        @yield('pageHead')
+    </head>
+    <body>
+        <!-- Errors and messages, if any
+        ('layouts._errors')
+        /errors-messages -->
+        
+        <div class="container">
+            @yield('content')
+        </div><!-- /.container -->
+
+        <!-- jQuery first, then Tether, then Bootstrap JS. -->
+        <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+        @yield('pageScript')
+    </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,115 @@
+@extends('layouts.app')
+
+@section('pageTitle', 'Glen\'s Tools')
+
+@section('content')
+
+<div class="card-deck">
+	<div class="card">
+		<div class="card-block">
+			<h4 class="card-title">Soffit Calculator</h4>
+			<p class="card-text">Calculate the Soffit Details.</p>
+			
+			<div class="form-group">
+				<label for="soffitLinearFeet">Linear Feet</label>
+				<input type="number" class="form-control" id="soffitLinearFeet" aria-describedby="soffitLinearFeetHelp" placeholder="Feet">
+				<small id="soffitLinearFeetHelp" class="form-text text-muted">Enter how many linear feet you're estimating for your project.</small>
+			</div>
+			<div class="form-group">
+				<label for="soffitDepth">Soffit Depth</label>
+				<input type="number" class="form-control" id="soffitDepth" aria-describedby="soffitDepthHelp" placeholder="Inches">
+				<small id="soffitDepthHelp" class="form-text text-muted">Enter how many inches you're estimating for your project.</small>
+			</div>
+		</div>
+		<div class="card-footer">
+			<div class="card-text">
+				Pieces: 
+				<span id="soffitPiecesResult">
+					0
+				</span>
+			</div>
+			<div class="card-text">
+				Sheets: 
+				<span id="soffitSheetsResult">
+					0
+				</span>
+			</div>
+			<div class="card-text">
+				Pieces per Sheet:
+				<span id="soffitPiecesPerSheetResult">
+					0
+				</span>
+			</div>
+		</div>
+	</div>
+	<div class="card">
+		<div class="card-block">
+			<h4 class="card-title">Card title</h4>
+			<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+			<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+		</div>
+	</div>
+</div>
+
+@endsection
+
+
+@section('pageScript')
+<script>
+$('#soffitLinearFeet').change( function() {
+	updateSoffit();
+});
+
+$('#soffitDepth').change( function() {
+	updateSoffit();
+});
+
+function updateSoffit()
+{
+	var linearFeet = $('#soffitLinearFeet');
+	var soffitDepth = $('#soffitDepth');
+
+	var	r = 0,
+		soffitPieces = 0,
+		soffitSheets = 0,
+		soffitPiecesPerSheet = 0;
+
+	// Check to see if two fields have numbers
+	if ( 	$.isNumeric( linearFeet.val() ) &&
+			$.isNumeric( soffitDepth.val() ) &&
+			linearFeet.val() > 0 &&
+			soffitDepth.val() > 0 )
+	{
+
+		soffitPieces = Math.round( linearFeet.val() * 12 / 16 * 100 ) / 100;
+
+		soffitPiecesPerSheet = Math.round( 144 / soffitDepth.val() * 100 ) / 100;
+
+		soffitSheets = Math.round( soffitPieces / soffitPiecesPerSheet * 100 ) / 100;
+
+		// alert( 'Linear Feet: ' + linearFeet.val() + '; \n' +
+		// 		'Soffit Depth: ' + soffitDepth.val() + '\n' +
+		// 		'Soffit Pieces: ' + soffitPieces  + '\n' +
+		// 		'Soffit Pieces per Sheet: ' + soffitPiecesPerSheet  + '\n' +
+		// 		'Soffit Sheets: ' + soffitSheets
+		// 		 );
+
+		soffitSetResults(	soffitPieces,
+							soffitPiecesPerSheet,
+							soffitSheets );
+	}
+}
+
+function soffitSetResults( pieces, piecesPerSheet, soffitSheets )
+{
+	$('#soffitPiecesResult').text(	Math.ceil( pieces ) +
+									' (' + pieces + ')' );
+
+	$('#soffitSheetsResult').text(	Math.ceil( piecesPerSheet ) +
+									' (' + piecesPerSheet + ')' );
+	$('#soffitPiecesPerSheetResult').text(	Math.floor( soffitSheets ) +
+											' (' + soffitSheets + ')' );
+}
+
+</script>
+@endsection
